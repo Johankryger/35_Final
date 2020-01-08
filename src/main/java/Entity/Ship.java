de75f46a44af95d9ac0817ea1dc3193feabe9c61
Colle <s195490@student.dtu.dk>
2020-01-08 14:20:14 +0100

    ship squares

---
+++ src/main/java/Entity/Ship.java
@@ -0,0 +1,33 @@
+package Entity;
+
+import Controller.GUIController;
+
+public class Ship extends Property{
+
+    public Ship(int fieldPosition, String fieldName, int price, int rent) {
+        super(fieldPosition, fieldName, price, rent);
+    }
+
+    @Override
+    public void squareAction(PlayerList playerList, GUIController gui, int diceSum) {
+        if (owner.equals("bank")) {
+            String option = gui.button("Want to buy " + fieldName + "?", "Buy", "Auction");
+            if (option.equals("Buy")) {
+                playerList.getPlayer().getBalance().pay(price);
+                gui.buyStreet(playerList.getPlayer().getName(), fieldPosition);
+                setOwner(playerList.getPlayer().getName(),playerList.getPlayer().getFieldPos());
+            }
+            else {
+                //setAuction(True, fieldPosition);
+                //gui.auction(fieldPosition);
+            }
+        }
+        else if (!owner.equals(playerList.getPlayer().getName())){
+            for (int i = 0; i < ; i++) {
+                if (owner.equals)
+                shipCounter++;
+            }
+            playerList.transfer(price,playerList.getPlayer().getName(),owner);
+        }
+    }
+}
