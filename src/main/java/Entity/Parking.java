4ceecf4050185a7aacc6c9a882a9e8cffe449135
Bendorff <andreasbendorff@hotmail.com>
2020-01-08 10:42:08 +0100

    Start på Squares og deres effekter

---
+++ src/main/java/Entity/Parking.java
@@ -0,0 +1,14 @@
+package Entity;
+
+public class Parking extends Square {
+
+
+    public Parking(int fieldPosition, String fieldName) {
+        super(fieldPosition, fieldName);
+    }
+
+    @Override
+    public void squareAction() {
+
+    }
+}
