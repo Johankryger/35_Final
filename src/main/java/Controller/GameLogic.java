977b231450918aca9ba6cf84bed01293e666465e
Colle <s195490@student.dtu.dk>
2020-01-09 11:39:48 +0100

    Gamelogic klasse

---
+++ src/main/java/Controller/GameLogic.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import Controller.GUIController;
+import Entity.DiceCup;
+import Entity.PlayerList;
+import Entity.SquareList;
+
+public class GameLogic {
+    private GUIController guiController = new GUIController();
+    private DiceCup diceCup = new DiceCup();
+    private PlayerList playerList = new PlayerList();
+    private SquareList squareList = new SquareList();
+
+    public void start() {
+        //Returns a string array of names
+        String[] names = guiController.startMenu();
+        playerList.addPlayers(names, names.length);
+
+
+        while (true) {
+            // Throwing dice process
+            guiController.button(playerList.getPlayer().getName() + "'s tur.", "Kast terning");
+            diceCup.rollDice();
+            guiController.showDice(diceCup.getFaceValueArray());
+
+            // Movement process
+            int startPos = playerList.getPlayer().getFieldPos();
+            playerList.getPlayer().move(diceCup.getFaceValueSum(), true);
+            guiController.movePlayer(playerList.getPlayer().getName(), playerList.getPlayer().getBalance().getAmount(), startPos, playerList.getPlayer().getFieldPos());
+
+            // Land on and squarelist
+            squareList.getSquare(playerList.getPlayer().getFieldPos()).squareAction(playerList, guiController, diceCup.getFaceValueSum());
+
+//            if(playerList.getPlayer().hasGotChanceCard()){
+//                chancelist.drawCard();
+            //}
+            playerList.nextPlayer();
+        }
+
+
+    }
+}
