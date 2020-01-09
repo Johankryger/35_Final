f649bb11f7af4a433daddc61ffde8daa2920c713
Colle <s195490@student.dtu.dk>
2020-01-09 12:35:39 +0100

    some gamelogic for die pairs

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -9,33 +9,35 @@ public class Controller {
     private DiceCup diceCup = new DiceCup();
     private PlayerList playerList = new PlayerList();
     private SquareList squareList = new SquareList();
+    private GameLogic gameLogic = new GameLogic();
 
     public void start() {
+        gameLogic.turn();
         //Returns a string array of names
-        String[] names = guiController.startMenu();
-        playerList.addPlayers(names, names.length);
-
-
-        while (true) {
-           // Throwing dice process
-            guiController.button(playerList.getPlayer().getName() + "'s tur.", "Kast terning");
-            diceCup.rollDice();
-            guiController.showDice(diceCup.getFaceValueArray());
-
-            // Movement process
-            int startPos = playerList.getPlayer().getFieldPos();
-            playerList.getPlayer().move(diceCup.getFaceValueSum(), true);
-            guiController.movePlayer(playerList.getPlayer().getName(), playerList.getPlayer().getBalance().getAmount(), startPos, playerList.getPlayer().getFieldPos());
-
-            // Land on and squarelist
-            squareList.getSquare(playerList.getPlayer().getFieldPos()).squareAction(playerList,guiController,diceCup.getFaceValueSum());
-
-//            if(playerList.getPlayer().hasGotChanceCard()){
-//                chancelist.drawCard();
-            //}
-            playerList.nextPlayer();
-        }
-
-
+//        String[] names = guiController.startMenu();
+//        playerList.addPlayers(names, names.length);
+//
+//
+//        while (true) {
+//           // Throwing dice process
+//            guiController.button(playerList.getPlayer().getName() + "'s tur.", "Kast terning");
+//            diceCup.rollDice();
+//            guiController.showDice(diceCup.getFaceValueArray());
+//
+//            // Movement process
+//            int startPos = playerList.getPlayer().getFieldPos();
+//            playerList.getPlayer().move(diceCup.getFaceValueSum(), true);
+//            guiController.movePlayer(playerList.getPlayer().getName(), playerList.getPlayer().getBalance().getAmount(), startPos, playerList.getPlayer().getFieldPos());
+//
+//            // Land on and squarelist
+//            squareList.getSquare(playerList.getPlayer().getFieldPos()).squareAction(playerList,guiController,diceCup.getFaceValueSum());
+//
+////            if(playerList.getPlayer().hasGotChanceCard()){
+////                chancelist.drawCard();
+//            //}
+//            playerList.nextPlayer();
+//        }
+//
+//
     }
 }
--- src/main/java/Controller/GameLogic.java
+++ src/main/java/Controller/GameLogic.java
@@ -10,8 +10,9 @@ public class GameLogic {
     private DiceCup diceCup = new DiceCup();
     private PlayerList playerList = new PlayerList();
     private SquareList squareList = new SquareList();
+    private int pairCounter=0;
 
-    public void start() {
+    public void turn() {
         //Returns a string array of names
         String[] names = guiController.startMenu();
         playerList.addPlayers(names, names.length);
@@ -23,20 +24,45 @@ public class GameLogic {
             diceCup.rollDice();
             guiController.showDice(diceCup.getFaceValueArray());
 
-            // Movement process
-            int startPos = playerList.getPlayer().getFieldPos();
-            playerList.getPlayer().move(diceCup.getFaceValueSum(), true);
-            guiController.movePlayer(playerList.getPlayer().getName(), playerList.getPlayer().getBalance().getAmount(), startPos, playerList.getPlayer().getFieldPos());
+            //Sets extraTurn to true/false depending on getPair method
+            playerList.getPlayer().extraTurn(getPair());
+            //sets player in jail if rolled pair 3 times in a row.
+            if (pairCounter==3){
+                playerList.getPlayer().setInJail(true);
+                playerList.getPlayer().setFieldPos(10);
+                playerList.nextPlayer();
+                pairCounter=0;
+            }
 
-            // Land on and squarelist
-            squareList.getSquare(playerList.getPlayer().getFieldPos()).squareAction(playerList, guiController, diceCup.getFaceValueSum());
+            else {
+                // Movement process
+                int startPos = playerList.getPlayer().getFieldPos();
+                playerList.getPlayer().move(diceCup.getFaceValueSum(), true);
+                guiController.movePlayer(playerList.getPlayer().getName(), playerList.getPlayer().getBalance().getAmount(), startPos, playerList.getPlayer().getFieldPos());
+
+                // Land on and squarelist
+                squareList.getSquare(playerList.getPlayer().getFieldPos()).squareAction(playerList, guiController, diceCup.getFaceValueSum());
 
 //            if(playerList.getPlayer().hasGotChanceCard()){
 //                chancelist.drawCard();
-            //}
-            playerList.nextPlayer();
+                //}
+                //gives turn to next player if extraTurn is false
+                if (!playerList.getPlayer().isExtraTurn()) {
+                    playerList.nextPlayer();
+                }
+            }
         }
+    }
 
-
+    //method for checking pair in dices
+    public boolean getPair() {
+        int[] dicearr = diceCup.getFaceValueArray();
+//        int[] dicearr = {1,1};
+        if (!(dicearr[0] == dicearr[1])) {
+            pairCounter = 0;
+            return false;
+        }
+            pairCounter++;
+            return true;
     }
 }
