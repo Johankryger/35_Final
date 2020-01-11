72b4ff21507ba4d4e33bf44d37cf1461c98c856b
max <maxmagi@hotmail.com>
2020-01-11 13:13:13 +0100

    getSquare jail fix

--- src/main/java/Entity/Jail.java
+++ src/main/java/Entity/Jail.java
@@ -11,8 +11,9 @@ public class Jail extends Square {
     @Override
     public void squareAction(PlayerList playerList, GUIController gui, int diceSum) {
         int startPos = playerList.getPlayer().getFieldPos();
-        playerList.getPlayer().setFieldPos(30);
-        gui.movePlayer(playerList.getPlayer().getName(),playerList.getPlayer().getBalance().getAmount(),startPos, 30);
+        playerList.getPlayer().setFieldPos(10);
+        gui.movePlayer(playerList.getPlayer().getName(),playerList.getPlayer().getBalance().getAmount(),startPos, 10);
         playerList.getPlayer().setInJail(true);
+        gui.button("You've been jailed!", "OK :(");
     }
 }
