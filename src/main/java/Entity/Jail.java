1965f09ccd7e99eb9d13de448a4e922fe4e09d37
max <maxmagi@hotmail.com>
2020-01-11 13:59:18 +0100

    jail square userfriendly message

--- src/main/java/Entity/Jail.java
+++ src/main/java/Entity/Jail.java
@@ -10,10 +10,10 @@ public class Jail extends Square {
 
     @Override
     public void squareAction(PlayerList playerList, GUIController gui, int diceSum) {
+        gui.button("You've been jailed!", "OK :(");
         int startPos = playerList.getPlayer().getFieldPos();
         playerList.getPlayer().setFieldPos(10);
         gui.movePlayer(playerList.getPlayer().getName(),playerList.getPlayer().getBalance().getAmount(),startPos, 10);
         playerList.getPlayer().setInJail(true);
-        gui.button("You've been jailed!", "OK :(");
     }
 }
