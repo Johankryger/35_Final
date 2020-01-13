57b5717aaaf67fdb0fd6192110ffcadaf4a7152a
Christian Merithz <cunnn98@gmail.com>
2020-01-13 12:04:55 +0100

    Updated controller to get methods from GameController instead of GameLogic.

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -4,83 +4,28 @@ import message.Message;
 
 public class Controller {
     private Message message = new Message("Dansk");
-    private GameController gameLogic = new GameController();
+    private GameController gameController = new GameController();
 
     public void start() {
-        gameLogic.turn();
+        gameController.turn();
         while(true) {
-            gameLogic.checkForLoser();
-            gameLogic.startTurn();
+            gameController.checkForLoser();
             String option = "manage bygninger";
             while (option.equals("manage bygninger")) {
-                gameLogic.updateProperties();
-                option = gameLogic.menu();
+                gameController.updateProperties();
+                option = gameController.menu();
             }
-            gameLogic.logicRollDice();
-            gameLogic.getPair();
-            gameLogic.searchForJail();
-            if (gameLogic.getPlayer().isInJail()){
-                gameLogic.nextPlayer();
+            gameController.rollDiceLogic();
+            gameController.jailLogic(option);
+            if (gameController.getPlayer().isInJail()){
+                gameController.nextPlayer();
             }
             else{
-                gameLogic.movePlayer();
-                gameLogic.landOn();
-                if (!gameLogic.getPlayer().hasExtraTurn()){
-                    gameLogic.nextPlayer();
+                gameController.movePlayer();
+                if (!gameController.getPlayer().hasExtraTurn()){
+                    gameController.nextPlayer();
                 }
             }
         }
-        //Returns a string array of names
-//        String[] names = guiController.startMenu();
-//        playerList.addPlayers(names, names.length);
-//
-//
-//        while (true) {
-//           // Throwing dice process
-//            guiController.button(playerList.getPlayer().getName() + "'s tur.", "Kast terning");
-//            diceCup.rollDice();
-//            guiController.showDice(diceCup.getFaceValueArray());
-//
-//            // Movement process
-//            int startPos = playerList.getPlayer().getFieldPos();
-//            playerList.getPlayer().move(diceCup.getFaceValueSum(), true);
-//            guiController.movePlayer(playerList.getPlayer().getName(), playerList.getPlayer().getBalance().getAmount(), startPos, playerList.getPlayer().getFieldPos());
-//
-//            // Land on and squarelist
-//            squareList.getSquare(playerList.getPlayer().getFieldPos()).squareAction(playerList,guiController,diceCup.getFaceValueSum());
-//
-////            if(playerList.getPlayer().hasGotChanceCard()){
-////                chancelist.drawCard();
-//            //}
-//            playerList.nextPlayer();
-//        }
-//
-//
-//        String[] names = guiController.startMenu();
-//        playerList.addPlayers(names, names.length);
-//
-//
-//        while (true) {
-//            propertyController.manageMenu(guiController, playerList, squareList);
-//           // Throwing dice process
-//            guiController.button(playerList.getPlayer().getName() + "'s tur.", "Kast terning");
-//            diceCup.rollDice();
-//            guiController.showDice(diceCup.getFaceValueArray());
-//
-//            // Movement process
-//            int startPos = playerList.getPlayer().getFieldPos();
-//            playerList.getPlayer().move(diceCup.getFaceValueSum(), true);
-//            guiController.movePlayer(playerList.getPlayer().getName(), playerList.getPlayer().getBalance().getAmount(), startPos, playerList.getPlayer().getFieldPos());
-//
-//            // Land on and squarelist
-//            squareList.getSquare(playerList.getPlayer().getFieldPos()).squareAction(playerList,guiController,diceCup.getFaceValueSum());
-//
-////            if(playerList.getPlayer().hasGotChanceCard()){
-////                chancelist.drawCard();
-//            //}
-//            playerList.nextPlayer();
-//        }
-//
-//
     }
 }
