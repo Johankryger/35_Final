18659c35d9e8b598ea2bf27b85c9fde8275b800b
Bendorff <andreasbendorff@hotmail.com>
2020-01-13 14:01:01 +0100

    metode 8 og 9 tilføjet til decket (testet)

--- src/main/java/Entity/chance/CardType8.java
+++ src/main/java/Entity/chance/CardType8.java
@@ -3,7 +3,7 @@ package Entity.chance;
 import Controller.GUIController;
 import Entity.PlayerList;
 import Entity.SquareList;
-
+// metode brugt til a opkræve et antal penge for hert hus eller hotel spilleren ejer
 public class CardType8 extends ChanceCard {
     private int houseTax;
     private int hotelTax;
