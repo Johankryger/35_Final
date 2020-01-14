d04c16cc1df2ae69d0e853aec518eff0eb6c5271
Johan Kryger <johankrygerr@gmail.com>
2020-01-14 12:39:07 +0100

    Hello new trading

--- src/main/java/Entity/Trade.java
+++ src/main/java/Entity/Trade.java
@@ -1,5 +1,30 @@
 package Entity;
 
+import Entity.square.SquareList;
+import staticclasses.ArrayMethods;
+
 public class Trade {
 
+    public String[] trade(String playerName, SquareList squareList){
+        String[] owendStreets = squareList.getOwnedStreetNames(playerName);
+        String[] ownedShips = squareList.getOwnedShipNames(playerName);
+        String[] ownedBreweries = squareList.getOwnedBreweryNames(playerName);
+        String[] tradeMenu  = new String[3];
+        tradeMenu[0] = "Go back";
+        tradeMenu[1] = "Next";
+        tradeMenu[2] = "Money offer";
+
+        for (int i = 0; i < owendStreets.length; i++) {
+            if (!squareList.checkAnyHouse(squareList.searchStreet(owendStreets[i]),playerName)){
+                tradeMenu = ArrayMethods.addToArray(tradeMenu,owendStreets[i]);
+            }
+        }
+        for (int i = 0; i < ownedShips.length; i++) {
+            tradeMenu = ArrayMethods.addToArray(tradeMenu,ownedShips[i]);
+        }
+        for (int i = 0; i < ownedBreweries.length; i++) {
+            tradeMenu = ArrayMethods.addToArray(tradeMenu,ownedBreweries[i]);
+        }
+        return tradeMenu;
+    }
 }
