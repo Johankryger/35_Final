f0e6a7900c0b07b17619c717482418fc155c7d48
Christian Merithz <cunnn98@gmail.com>
2020-01-14 16:09:06 +0100

    opdateret ship beskeder.

--- src/main/java/Entity/square/Ship.java
+++ src/main/java/Entity/square/Ship.java
@@ -2,6 +2,7 @@ package Entity.square;
 
 import Controller.GUIController;
 import Entity.PlayerList;
+import message.Message;
 import staticclasses.ArrayMethods;
 
 public class Ship extends Property{
@@ -17,8 +18,8 @@ public class Ship extends Property{
     @Override
     public void squareAction(PlayerList playerList, GUIController gui, int diceSum) {
         if (owner.equals("bank")) {
-            String option = gui.button("Want to buy " + fieldName + "?", "Buy", "Auction");
-            if (option.equals("Buy")) {
+            String option = gui.button((Message.getMessage("General",1)) + fieldName + (Message.getMessage("General",2)), (Message.getMessage("General",3)), (Message.getMessage("General",4)));
+            if (option.equals((Message.getMessage("General",3)))) {
                 playerList.getPlayer().getBalance().pay(price);
                 gui.buyProperty(playerList.getPlayer().getName(), fieldPosition);
                 owner = playerList.getPlayer().getName();
@@ -29,7 +30,7 @@ public class Ship extends Property{
             }
         }
         else if (!owner.equals(playerList.getPlayer().getName())){
-            gui.button("You pay " + rent + " to " + owner, "Okay");
+            gui.button((Message.getMessage("General",5)) + rent + (Message.getMessage("General",6)) + owner, (Message.getMessage("General",7)));
             playerList.transfer(rent,playerList.getPlayer().getName(),owner);
             gui.updateBalance(playerList.getPlayer().getName(), playerList.getPlayer().getBalance().getAmount());
             gui.updateBalance(owner, playerList.searchPlayer(owner).getBalance().getAmount());
