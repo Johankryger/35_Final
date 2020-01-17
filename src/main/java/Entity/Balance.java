621d08d0122bf796c70c56f851a76e4a2a11c245
MerithzDTU <s195480@student.dtu.dk>
2020-01-17 13:19:28 +0100

    JavaDoc comments finished for Balance.

--- src/main/java/Entity/Balance.java
+++ src/main/java/Entity/Balance.java
@@ -1,6 +1,12 @@
 package Entity;
 
+/**
+ * Balance class used for adding and paying amounts.
+ */
 public class Balance {
+    /**
+     * the value of the player's gamestart balance.
+     */
     private int amount = 30000;
 
     public Balance () {
@@ -11,10 +17,18 @@ public class Balance {
         return amount;
     }
 
+    /**
+     * Method that pays an amount and redraws it from the current balance amount.
+     * @param amount the amount that the player needs to pay.
+     */
     public void pay(int amount) {if (!(this.amount<amount) && amount > 0)
         this.amount = this.amount - amount;
     }
 
+    /**
+     * Method that adds an amount to the current balance amount.
+     * @param amount the amount that is added to the players balance.
+     */
     public void add(int amount) {
         if (!(amount<0)) this.amount = this.amount + amount;
     }
