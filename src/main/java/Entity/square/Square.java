8c6c4263c01b2188073d2e6f446365dcadde0646
MerithzDTU <s195480@student.dtu.dk>
2020-01-17 13:27:09 +0100

    JavaDoc comments finished for Square

--- src/main/java/Entity/square/Square.java
+++ src/main/java/Entity/square/Square.java
@@ -4,11 +4,24 @@ import Controller.GUIController;
 import Controller.PropertyController;
 import Entity.PlayerList;
 
+/**
+ * Abstract class for all squares.
+ */
 public abstract class Square {
+    /**
+     * Position of the field on the board.
+     */
     protected int fieldPosition;
+    /**
+     * Name of the field.
+     */
     protected String fieldName;
 
-
+    /**
+     * Square constructor, which contains name and position of the field, which is common for all squares.
+     * @param fieldPosition Position of the field on the board.
+     * @param fieldName Name of the field.
+     */
     public Square(int fieldPosition, String fieldName) {
         this.fieldName = fieldName;
         this.fieldPosition = fieldPosition;
@@ -21,6 +34,14 @@ public abstract class Square {
         return fieldName;
     }
 
+    /**
+     * Abstract method of the action the square has.
+     * @param playerList object of PlayerList
+     * @param gui object of GUIController
+     * @param propertyController object of PropertyController
+     * @param squareController object of SquareController
+     * @param diceSum integer of dicesum
+     */
     public abstract void squareAction(PlayerList playerList, GUIController gui, PropertyController propertyController, SquareController squareController, int diceSum);
 
 }
